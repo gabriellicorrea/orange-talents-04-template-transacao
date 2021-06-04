f8a216e93994c2cbd777c13e6cc8ee522102acf9
Gabrielli Correa <gabriellicorrea7@gmail.com>
2021-06-04 10:29:04 -0300

    00.setup-projeto

---
+++ src/main/java/br/com/zup/gabrielli/GabrielliApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.gabrielli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabrielliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabrielliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/gabrielli/GabrielliApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.gabrielli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabrielliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
